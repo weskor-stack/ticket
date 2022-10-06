82bc27c9eafa92239cbc7fd0ac00277a999b064b
Edgar <weskorgenesis@gmail.com>
2022-10-06 13:11:08 -0500

    Update views

--- resources/views/service/index.blade.php
+++ resources/views/service/index.blade.php
@@ -60,31 +60,7 @@
                             </span>
 
                              <div class="float-right">
-                                <style>
-                                    .myButton {
-                                        box-shadow: 3px 4px 0px 0px #899599;
-                                        background:linear-gradient(to bottom, #ededed 5%, #bab1ba 100%);
-                                        background-color:#ededed;
-                                        border-radius:15px;
-                                        border:1px solid #d6bcd6;
-                                        display:inline-block;
-                                        cursor:pointer;
-                                        color:#000000;
-                                        font-family:Arial;
-                                        font-size:17px;
-                                        padding:10px 16px;
-                                        text-decoration:none;
-                                        text-shadow:0px 1px 0px #e1e2ed;
-                                    }
-                                    .myButton:hover {
-                                        background:linear-gradient(to bottom, #bab1ba 5%, #ededed 100%);
-                                        background-color:#bab1ba;
-                                    }
-                                    .myButton:active {
-                                        position:relative;
-                                        top:1px;
-                                    }
-                                </style>
+                                <link rel="stylesheet" href="{{ asset('css/CSS_Service_order/CSS_Service_order.css') }}">
                                     @if($service->service_status_id=='2')
                                         <a href="{{ route('service.pdf','id_ticket='.$service->order_service_id) }}" class="btn btn-primary btn-lg"  data-placement="left" title="{{ __('PDF') }}"><i class="material-icons">book</i>&nbsp; {{ __('PDF') }}</a>
 &nbsp;
