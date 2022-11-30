22723e0dbdc5b0033f7d6d56d004c8c2fd1bd966
weskor-stack <54750110+weskor-stack@users.noreply.github.com>
2022-11-30 09:49:46 -0600

    update warranty

--- resources/views/Warranty/index.blade.php
+++ resources/views/Warranty/index.blade.php
@@ -8,10 +8,7 @@
 
 @section('content')
 <script src="{{ asset('js/warranty_js/Warranty_js.js')}}" defer></script>
-<link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
-<link rel="stylesheet" href="/resources/demos/style.css">
-<script src="https://code.jquery.com/jquery-3.6.0.js"></script>
-<script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>  
+
 {{-- /////////////////////////////////////////////////////////////////// div principal /////////////////////////////////////////////////////////////////////// --}}  
 <div>
 
